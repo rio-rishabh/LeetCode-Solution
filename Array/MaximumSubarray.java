5e55e050388a78a539c49b95746c1d33ff4a8608
Rishabh Sharma <riorishabh448@gmail.com>
2026-01-07 23:35:39 -0500

    added MaximumSubarray to the solved Problems

---
+++ Array/MaximumSubarray.java
@@ -0,0 +1,19 @@
+public class MaximumSubarray {
+    public static void main(String args[]){
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        if(arr.length == 0){
+            System.out.println("Array is empty");
+        }
+        for(int i = 1; i < arr.length; i++){
+         
+            currentSum = Math.max(arr[i],  arr[i] + currentSum);
+
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        System.out.println("Maximum sum is: " + maxSum);
+    }
+}
