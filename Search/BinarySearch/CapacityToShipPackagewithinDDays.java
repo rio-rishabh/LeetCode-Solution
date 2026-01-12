34b3c1921bcc9d5b0a985967a80bf7cd69b364b6
Rishabh Sharma <riorishabh448@gmail.com>
2026-01-12 04:36:20 -0500

    Added Capacity to SHip to solved problems

---
+++ Search/BinarySearch/CapacityToShipPackagewithinDDays.java
@@ -0,0 +1,54 @@
+public class CapacityToShipPackagewithinDDays {
+    public static void main(String args[]){
+        int weights[] = {1,2,3,4,5,6,7,8,9,10};
+        int days = 5;
+
+        int left  = findMax(weights);
+        int right = findSum(weights);
+
+        while(left < right){
+            int mid = left + (right -left) / 2;
+
+            if(canShip(weights, mid, days)){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        System.out.println("The minimum capacity is: " + left);
+    }
+
+    public static int findMax(int weights[]){
+        int max = 0;
+
+        for(int weight : weights){
+            max = Math.max(max, weight);
+        }
+        return max;
+    }
+
+    public static boolean canShip(int weights[], int capacity, int days){
+        int daysNeeded  = 1;
+        int currentWeight = 0;
+
+        for(int weight : weights){
+            if(currentWeight + weight > capacity){
+                daysNeeded ++;
+                currentWeight = weight;
+            }
+            else{
+                currentWeight += weight;
+            }
+        }
+        return daysNeeded <= days;
+    }
+
+    public static int findSum(int weights[]){
+        int sum = 0;
+        for(int weight : weights){
+            sum += weight;
+        }
+        return sum;
+    }
+}
