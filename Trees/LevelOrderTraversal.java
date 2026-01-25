4981d0ccf88b167b6104fcfba7d6b80dc7085e92
Rishabh Sharma <riorishabh448@gmail.com>
2026-01-24 19:47:35 -0500

    Added Level Order Traversal in solved Problems

---
+++ Trees/LevelOrderTraversal.java
@@ -0,0 +1,55 @@
+package Trees;
+
+import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LevelOrderTraversal {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    static List<List<Integer>> levelOrderTraversal(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for(int i = 0; i < levelSize; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if(node.left != null){ queue.add(node.left);}
+                if(node.right != null){queue.add(node.right);}
+            }
+
+            result.add(level);
+        }
+        return result;
+    }
+
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+        List<List<Integer>> result = levelOrderTraversal(root);
+        System.out.println("Level order traversal: " + result);
+    }
+}
