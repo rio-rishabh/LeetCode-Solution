3664f25f6a7fc1beb4918b6d8e6dcb7467b6576a
Rishabh Sharma <riorishabh448@gmail.com>
2026-01-27 19:34:44 -0500

    Added MaximumDepth of Binary Tree to solved Problems

--- Trees/MaximumDepthOFBinaryTree.java
+++ Trees/MaximumDepthOFBinaryTree.java
@@ -1,11 +1,11 @@
 package Trees;
 
-import java.util.Queue;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.LinkedList;
 
-public class LevelOrderTraversal {
+public class MaximumDepthOFBinaryTree {
     static class TreeNode{
         int val;
         TreeNode left;
@@ -16,31 +16,33 @@ public class LevelOrderTraversal {
         }
     }
 
-    static List<List<Integer>> levelOrderTraversal(TreeNode root){
-        List<List<Integer>> result = new ArrayList<>();
+    static int maxDepth(TreeNode root){
         if(root == null){
-            return result;
+            return 0;
         }
+
+        List<List<Integer>> result = new ArrayList<>();
+
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+        int depth = 0;
 
         while(!queue.isEmpty()){
             int levelSize = queue.size();
+            depth++;
             List<Integer> level = new ArrayList<>();
-
-            for(int i = 0; i < levelSize; i++){
+            
+            for(int i = 0 ; i< levelSize; i++){
                 TreeNode node = queue.poll();
                 level.add(node.val);
 
-                if(node.left != null){ queue.add(node.left);}
+                if(node.left != null){queue.add(node.left);}
                 if(node.right != null){queue.add(node.right);}
             }
-
             result.add(level);
         }
-        return result;
+        return depth;
     }
-
     public static void main(String args[]){
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
@@ -49,7 +51,7 @@ public class LevelOrderTraversal {
         root.left.right = new TreeNode(3);
         root.right.left = new TreeNode(5);
         root.right.right = new TreeNode(7);
-        List<List<Integer>> result = levelOrderTraversal(root);
-        System.out.println("Level order traversal: " + result);
+        int result = maxDepth(root);
+        System.out.println("Maximum depth of binary tree is: " + result);
     }
 }
