096f266baf0c3db2a71650b21a343848d72ca0ee
Rishabh Sharma <riorishabh448@gmail.com>
2026-02-14 23:49:25 -0500

    added Course Schedule 2 to solved Problems

--- Graphs/CourseSchedule2.java
+++ Graphs/CourseSchedule2.java
@@ -2,40 +2,44 @@ package Graphs;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Arrays;
 
-public class CourseSchedule {
-    
+public class CourseSchedule2 {
     static int [] state;
-
     static List<List<Integer>> graph;
-    public static boolean canFinish(int numCourses, int prerequisites[][]){
-        if(numCourses <=0 || prerequisites == null || prerequisites.length == 0){
-            return true;
+    static List<Integer> result;
+
+    public static int[] findOrder(int numCourses, int prerequisities[][]){
+        if(numCourses <= 0 || prerequisities == null || prerequisities.length ==0){
+            return new int[0];
         }
 
         state = new int[numCourses];
         graph = new ArrayList<>();
 
-        for(int i = 0 ; i< numCourses ; i++){
+        for(int i = 0 ; i < numCourses ; i++){
             graph.add(new ArrayList<>());
         }
 
-        for(int prerequisite[]: prerequisites){
+        for(int prerequisite[] : prerequisities){
             int course = prerequisite[0];
             int preCourse = prerequisite[1];
 
             graph.get(preCourse).add(course);
         }
 
-        for(int course = 0 ; course < numCourses ; course ++){
+        for(int course = 0 ; course < numCourses; course ++){
             if(hasCycle(course)){
-                return false;
+                return new int[0];
             }
         }
-        return true;
+        Collections.reverse(result);
+        return result.stream().mapToInt(i -> i).toArray();
     }
 
     public static boolean hasCycle(int course){
+
         if(state[course] == 1){
             return true;
         }
@@ -44,21 +48,21 @@ public class CourseSchedule {
             return false;
         }
         state[course] = 1;
-
+        
         for(int nextCourse : graph.get(course)){
-
             if(hasCycle(nextCourse)){
                 return true;
             }
         }
         state[course] = 2;
+        result.add(course);
         return false;
     }
 
     public static void main(String args[]){
-        int prerequisites[][] = {{1,0},{0,1}};
-        int numCourses = 2;
-        boolean result = canFinish(numCourses, prerequisites);
-        System.out.println("Can finish all courses: " + result);
+        int prerequisities[][] = {{1,0},{2,0},{3,1},{3,2}};
+        int numCourses = 4;
+        int result[] = findOrder(numCourses, prerequisities);
+        System.out.println("Course order: " + Arrays.toString(result));
     }
 }
