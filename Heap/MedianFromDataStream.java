a210b8d1ef4aebfd97f93f67ed9b3a5405704d30
Rishabh Sharma <riorishabh448@gmail.com>
2026-03-03 16:27:15 -0500

    added Median From Data Stream  to solved problem

--- Heap/MedianFromDataStream.java
+++ Heap/MedianFromDataStream.java
@@ -1,5 +1,39 @@
 package Heap;
 
+import java.util.PriorityQueue;
 public class MedianFromDataStream {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b - a);
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b);
 
+    public void addNum(int num){
+        if(maxHeap.isEmpty() || num <= maxHeap.peek()){
+            maxHeap.offer(num);
+        }
+        else{
+            minHeap.offer(num);
+        }
+        if(maxHeap.size() > minHeap.size() + 1){
+            minHeap.offer(maxHeap.poll());
+        }
+        else if(maxHeap.size() < minHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian(){
+        if(maxHeap.size() == minHeap.size()){
+            return (maxHeap.peek() + minHeap.peek()) / 2;
+        }
+        else{
+            return maxHeap.peek();
+        }
+    }
+
+    public static void main(String args[]){
+        MedianFromDataStream medianFromDataStream = new MedianFromDataStream();
+        medianFromDataStream.addNum(1);
+        medianFromDataStream.addNum(2);
+        medianFromDataStream.addNum(3);
+        System.out.println("Median is: " + medianFromDataStream.findMedian());
+    }
 }
