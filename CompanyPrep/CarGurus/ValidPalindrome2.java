81b4f231269e8bd268ace40c9c3295f15295525a
Rishabh Sharma <riorishabh448@gmail.com>
2026-06-02 23:38:57 -0400

    adding ValidPalindrome only after removing a character from string

---
+++ CompanyPrep/CarGurus/ValidPalindrome2.java
@@ -0,0 +1,36 @@
+public class ValidPalindrome2 {
+
+    public static void main(String args[]){
+        String s = "abbca";
+        boolean result = isPalindrome(s);
+        System.out.println("Is the string a valid palindrome? " + result);
+    }
+
+
+    public static boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return isPalindrome(s, left+1 , right ) || isPalindrome(s, left, right -1);
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
