823fd24b49e352ac4c5b2f56bac42708ff61ccc8
Rishabh Sharma <riorishabh448@gmail.com>
2026-06-19 21:23:57 -0400

    adding maximum Depth OF Binary Tree using BFS

--- BFS_DFS/MaximumDepthOfBinaryTree.java
+++ BFS_DFS/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,9 @@
 package BFS_DFS;
 
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ArrayList;
 public class MaximumDepthOfBinaryTree {
     public static class TreeNode{
         int val;
@@ -20,7 +24,7 @@ public class MaximumDepthOfBinaryTree {
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(7);
         root.left.left.left = new TreeNode(8);
-        int result = maxDepth(root);
+        int result = maxDepthBFS(root);
         System.out.println("The maximum depth of the binary tree is: " + result);
     }
 
@@ -31,5 +35,35 @@ public class MaximumDepthOfBinaryTree {
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+
+    // Other Apprach using BFS (Level Order Traversal)
+
+    public static int maxDepthBFS(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int depth = 0;
+        List<List<Integer>> result = new ArrayList<>();
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            depth ++;
+            List<Integer> level = new ArrayList<>();
+
+            for(int i =0 ; i< levelSize; i++){
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if(node.left != null){queue.add(node.left);}
+                if(node.right != null){queue.add(node.right);}
+            }
+
+            result.add(level);
+        }
+        return depth;
     }
 }
