9fa46ccacb7ab8bd9117b014bed42e199e29611d
Rishabh Sharma <riorishabh448@gmail.com>
2026-06-28 19:07:43 -0400

    adding Product of Self with Prefix and Suffix approach

--- CompanyPrep/Amazon/ProductOFArrayExceptSelf.java
+++ CompanyPrep/Amazon/ProductOFArrayExceptSelf.java
@@ -9,18 +9,40 @@ public class ProductOFArrayExceptSelf {
         System.out.println("The product of array except self is: " + Arrays.toString(result));
     }
 
+    // public static int[] productOfArrayExceptSelf(int arr[]){
+    //     int result[] = new int[arr.length];
+
+    //     result[0] = 1;
+    //     for(int i = 1 ; i < arr.length; i++){
+    //         result[i] = result[i-1] * arr[i-1];
+    //     }
+
+    //     int suffix = 1;
+    //     for(int i = arr.length - 1; i >= 0; i--){
+    //         result[i] = result[i] * suffix;
+    //         suffix *= arr[i];
+    //     }
+    //     return result;
+    // }
+
+
     public static int[] productOfArrayExceptSelf(int arr[]){
         int result[] = new int[arr.length];
+        int prefix[] = new int[arr.length];
+        int suffix[] = new int[arr.length];
 
-        result[0] = 1;
-        for(int i = 1 ; i < arr.length; i++){
-            result[i] = result[i-1] * arr[i-1];
+        prefix[0] = 1;
+        for(int i = 1; i< arr.length; i++){
+            prefix[i] = prefix[i-1] * arr[i-1];
         }
 
-        int suffix = 1;
-        for(int i = arr.length - 1; i >= 0; i--){
-            result[i] = result[i] * suffix;
-            suffix *= arr[i];
+        suffix[arr.length -1 ] = 1;
+        for(int i = arr.length - 2; i >= 0; i--){
+            suffix[i] = suffix[i+1] * arr[i + 1];
+        }
+
+        for(int i = 0 ; i< arr.length; i++){
+            result[i] = prefix[i] * suffix[i];
         }
         return result;
     }
