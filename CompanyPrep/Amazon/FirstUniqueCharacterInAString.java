720b6c5659c5f2c2347460388fda28939cc28efc
Rishabh Sharma <riorishabh448@gmail.com>
2026-07-30 12:59:14 -0400

    Using New Way to identify

--- CompanyPrep/Amazon/FirstUniqueCharacterInAString.java
+++ CompanyPrep/Amazon/FirstUniqueCharacterInAString.java
@@ -10,22 +10,37 @@ public class FirstUniqueCharacterInAString {
         System.out.println(result);
     }
 
-    public static int firstUniqueCharacter(String s){
-        Map<Character, Integer> frequency = new HashMap<>();
+    // public static int firstUniqueCharacter(String s){
+    //     Map<Character, Integer> frequency = new HashMap<>();
 
-        for(Character c : s.toCharArray()){
-            if(frequency.containsKey(c)){
-                frequency.put(c, frequency.getOrDefault(c, 0) + 1);
-            }
-            else{
-                frequency.put(c, 1);
-            }
+    //     for(Character c : s.toCharArray()){
+    //         if(frequency.containsKey(c)){
+    //             frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+    //         }
+    //         else{
+    //             frequency.put(c, 1);
+    //         }
+    //     }
+    //     for(int i = 0 ; i < s.length(); i++){
+    //         if(frequency.get(s.charAt(i)) == 1){
+    //             return i;
+    //         }
+    //         }
+    //         return -1;
+    // }
+
+    public static int firstUniqueCharacter(String s){
+        int freq[] = new int[26];
+
+        for(int i = 0 ; i< s.length(); i++){
+            freq[s.charAt(i) - 'a']++;
         }
-        for(int i = 0 ; i < s.length(); i++){
-            if(frequency.get(s.charAt(i)) == 1){
+
+        for(int i = 0 ; i< s.length(); i++){
+            if(freq[s.charAt(i) - 'a'] == 1){
                 return i;
-            }
-            }
-            return -1;
+        }
+    }
+        return -1;
     }
 }
