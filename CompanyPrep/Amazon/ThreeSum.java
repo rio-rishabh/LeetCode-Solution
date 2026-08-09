b3f58ba0611df6b50222d8615503b53a89b79b70
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-08 23:58:54 -0400

    adding threesum

---
+++ CompanyPrep/Amazon/ThreeSum.java
@@ -0,0 +1,42 @@
+package CompanyPrep.Amazon;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ThreeSum {
+    public static void main(String args[]){
+        int nums[] = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSum(int nums[]){
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i< n - 2; i++){
+            if(nums[i] > 0) break;
+
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i +1;
+            int right = n - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left ++;
+                    right --;
+                    while(left < right && nums[left] == nums[left-1]) left ++;
+                    while(left < right  && nums[right] == nums[right + 1]) right --;
+                }
+                else if(sum < 0){
+                    left ++;
+                }
+                else{
+                    right --;
+                }
+            }
+        }
+        return ans;
+    }
+}
