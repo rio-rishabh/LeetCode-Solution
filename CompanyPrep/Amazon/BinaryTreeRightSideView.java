1e185871e68dde3e53c11f5f6750edcbf00599c0
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-15 19:09:59 -0400

    Right Side view of Binary Tree

--- CompanyPrep/Amazon/BinaryTreeRightSideView.java
+++ CompanyPrep/Amazon/BinaryTreeRightSideView.java
@@ -1,5 +1,48 @@
 package CompanyPrep.Amazon;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Queue;
 public class BinaryTreeRightSideView {
 
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(4);
+        System.out.println(rightSideView(root));
+    }
+
+    public static List<Integer> rightSideView(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0 ; i< size; i++){
+                TreeNode node = queue.poll();
+                if(node.left != null){queue.offer(node.left);}
+                if(node.right != null){queue.offer(node.right);}
+
+                if(i == size - 1){
+                    result.add(node.val);
+                }
+            }
+        }
+        return result;
+    }
 }
