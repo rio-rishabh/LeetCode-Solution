afd24621942269f026e3e1ce38b9d4fae77ca916
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-15 20:23:53 -0400

    adding LowestCOmmon ANcestor of Binary Tree

---
+++ CompanyPrep/Amazon/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,41 @@
+package CompanyPrep.Amazon;
+
+public class LowestCommonAncestorOfBinaryTree {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val){
+            this.val = val;
+        }
+    }
+
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        TreeNode p = root.left.left.left;
+        TreeNode q = root.left.right.right;
+        System.out.println(lowestCommonAncestor(root, p, q).val);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left != null && right != null){
+            return root;
+        }
+        return left != null ? left : right;
+    }
+}
