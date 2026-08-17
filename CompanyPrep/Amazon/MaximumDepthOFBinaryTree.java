597ce869c483bbf194ee04f193b82f844a301140
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-16 22:28:47 -0400

    adding Maximum Depth of Binary Tree

---
+++ CompanyPrep/Amazon/MaximumDepthOFBinaryTree.java
@@ -0,0 +1,34 @@
+package CompanyPrep.Amazon;
+
+public class MaximumDepthOFBinaryTree {
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val){
+            this.val= val;
+        }
+    }
+
+    public static void main(String args[]){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+}
