76137431a25adf5d1e689f5056b37cd475a8d891
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-21 21:39:26 -0400

    adding KthLargest Element in Array

--- CompanyPrep/Amazon/KthLargestElementinAnArray.java
+++ CompanyPrep/Amazon/KthLargestElementinAnArray.java
@@ -1,5 +1,9 @@
 package CompanyPrep.Amazon;
 
 public class KthLargestElementinAnArray {
-
+    public static void main(String args[]){
+        int nums[] = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));
+    }
 }
