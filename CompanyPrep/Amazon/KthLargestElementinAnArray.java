cc5bc14be5f68f38d0675d963f96ff26cb00cb23
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-25 20:49:38 -0400

    adding QuickSelect way of finding largest element

--- CompanyPrep/Amazon/KthLargestElementinAnArray.java
+++ CompanyPrep/Amazon/KthLargestElementinAnArray.java
@@ -17,18 +17,53 @@ public class KthLargestElementinAnArray {
     // } // 0(nlogn) time complexity and 0(1) space complexity
 
 
+    // public static int findKthLargest(int nums[], int k){
+    //     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    //     for(int num : nums){
+    //         if(minHeap.size() < k){
+    //             minHeap.offer(num);
+    //         }
+    //         else if(num > minHeap.peek()){
+    //             minHeap.poll();
+    //             minHeap.offer(num);
+    //         }
+    //     }
+    //     return minHeap.peek(); // O(n logk) time complexity and O(k) space complexity
+    // }
+
     public static int findKthLargest(int nums[], int k){
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int num : nums){
-            if(minHeap.size() < k){
-                minHeap.offer(num);
+        int target = nums.length - k;
+        int lo =0, hi = nums.length - 1;
+        while(lo < hi){
+            int p = partition (nums, lo, hi);
+            if(p == target){return nums[p];}
+            if(p < target){
+                lo = p +1;
             }
-            else if(num > minHeap.peek()){
-                minHeap.poll();
-                minHeap.offer(num);
+            else if(p > target){
+                hi = p -1;
             }
         }
-        return minHeap.peek();
+        return -1;
+    }
+
+    public static int partition(int nums[], int lo , int high){
+        int pivot = nums[high];
+        int i = lo;
+        for(int j = lo ; j < high; j++){
+            if(nums[j] <= pivot){
+                swap(nums, i, j);
+                i++;
+            }
+        }
+        swap(nums, i, high);
+        return i;
+    }
+
+    public static void swap(int nums[], int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 
