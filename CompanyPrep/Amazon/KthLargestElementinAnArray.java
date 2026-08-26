bec47169e25e6d858115ba0c8e9b65634c973123
Rishabh Sharma <riorishabh448@gmail.com>
2026-08-25 20:31:01 -0400

    adding The Solution with Min-Heap

--- CompanyPrep/Amazon/KthLargestElementinAnArray.java
+++ CompanyPrep/Amazon/KthLargestElementinAnArray.java
@@ -2,6 +2,7 @@
 package CompanyPrep.Amazon;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthLargestElementinAnArray {
     public static void main(String args[]){
@@ -10,9 +11,24 @@ public class KthLargestElementinAnArray {
         System.out.println(findKthLargest(nums, k));
     }
 
+    // public static int findKthLargest(int nums[], int k){
+    //     Arrays.sort(nums);
+    //     return nums[nums.length - k];
+    // } // 0(nlogn) time complexity and 0(1) space complexity
+
+
     public static int findKthLargest(int nums[], int k){
-        Arrays.sort(nums);
-        return nums[nums.length - k];
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num : nums){
+            if(minHeap.size() < k){
+                minHeap.offer(num);
+            }
+            else if(num > minHeap.peek()){
+                minHeap.poll();
+                minHeap.offer(num);
+            }
+        }
+        return minHeap.peek();
     }
 }
 
