52d00480b3a13296174f9295e731605d2f7a7f93
Rishabh Sharma <riorishabh448@gmail.com>
2026-09-11 22:40:29 -0400

    adding TopKfrequentElements

---
+++ CompanyPrep/Amazon/TopKFrequentElements.java
@@ -0,0 +1,10 @@
+package CompanyPrep.Amazon;
+
+public class TopKFrequentElements {
+    public static void main(String args[]){
+        int nums[] = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(topKFrequent(nums, k));
+    }
+    public static int[] topKFrequent(int[] nums, int k) {}
+}
